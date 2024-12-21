f29d4fbf6d09463cd71f514f7663ea4a081723a8
SadhwaniMoomal <sadhwanimoomal987@gmail.com>
2024-12-21 14:30:45 +0500

    update file

---
+++ Menus/menuThemeOne/JS/script.js
@@ -0,0 +1,10 @@
+const img = document.querySelector('.users-img');
+const subMenu = document.querySelector('#sub-menu');
+
+img.addEventListener('click', () => {
+  subMenu.classList.toggle('open-meun');
+});
+
+
+
+
